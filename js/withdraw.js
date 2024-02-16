838ff2a98df06da90fd62e60be8f7e97c8c71b97
Tuhin531 <mdtuhinhossain153@gmail.com>
2024-02-16 01:47:33 +0100

    add new code for calculation

--- js/withdraw.js
+++ js/withdraw.js
@@ -16,18 +16,30 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     // change default string to number
     const perviousWithdrawTotal = parseFloat(perviousWithdrawTotalString);
     // add number total ballance
-    const currentWithdrawTotal = perviousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal ;
 
-    // clear the deposit input value
-    withdrawField.value = '';
+    
+  
 
 
     const balanceTotalElement = document.getElementById('balance-total');
     const perviousBalanceTotalString = balanceTotalElement.innerText;
     const perviousBalanceTotal = parseFloat(perviousBalanceTotalString);
+    
+    // clear the deposit input value
+    withdrawField.value = '';
+
+    if(newWithdrawAmount > perviousWithdrawTotal){
+        alert('Invalid Amount');
+
+        return;
+    }
+
+    const currentWithdrawTotal = perviousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal ;
+    
+
     const currentBalanceTotal = perviousBalanceTotal - newWithdrawAmount;
     // set the balance total
     balanceTotalElement.innerText = currentBalanceTotal;
-
+  
 })
