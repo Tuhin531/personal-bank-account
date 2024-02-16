775191f584d862c3512f5a4cc210348459ff3cc6
Tuhin531 <mdtuhinhossain153@gmail.com>
2024-02-16 01:00:16 +0100

    add code

--- js/deposit.js
+++ js/deposit.js
@@ -6,15 +6,23 @@ document.getElementById('btn-submit').addEventListener('click', function () {
     // for input filed use .value to get value inside the input 
 
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
+    const newDepositAmountString = depositField.value;
+    // change default string to number
+    const newDepositAmount = parseFloat(newDepositAmountString);
     // console.log(depositAmount);
 
     // step 3. get the current deposit total
     // for non-input element other than input or textarea use innerText to get the text
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+    const perviousDepositTotalString = depositTotalElement.innerText;
+    // change default string to number
+    const perviousDepositTotal = parseFloat(perviousDepositTotalString);
+
+    const currentDepositTotal = perviousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal;
     
+    // clear the deposit input value
+    depositField.value = '';
 
 
 
